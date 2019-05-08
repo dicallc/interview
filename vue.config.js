41c00d886d83dd2f6d7d666c123884ede57cc1f8
jiangdk <jiangdk@e-lead.cn>
2019-05-08 22:23:36 +0800

    提交代码~~~~~~~~~~~·

--- vue.config.js
+++ vue.config.js
@@ -26,8 +26,8 @@ const webpackHtmlOptions = {
     'vue': 'Vue',
     'vue-router': 'VueRouter',
     'vuex': 'Vuex',
-    'muse-ui': 'MuseUi',
-    'marked': 'Marked',
+    'muse-ui': 'MuseUI',
+    // 'marked': 'Marked',
     'mavon-editor': 'MavonEditor',
     'js-cookie': 'Cookies'
   },
@@ -38,12 +38,12 @@ const webpackHtmlOptions = {
       ],
       js: [
         'https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.min.js',
-        'https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router.min.js',
-        'https://unpkg.com/vuex@3.0.1/dist/vuex.min.js',
         'https://unpkg.com/muse-ui/dist/muse-ui.js',
-        'https://cdn.jsdelivr.net/npm/marked/marked.min.js',
+        // 'https://cdn.jsdelivr.net/npm/marked/marked.min.js',
         'https://cdn.jsdelivr.net/npm/mavon-editor@2.7.4/dist/mavon-editor.min.js',
-        'https://cdn.jsdelivr.net/npm/js-cookie@2.2.0/src/js.cookie.min.js'
+        'https://cdn.jsdelivr.net/npm/js-cookie@2.2.0/src/js.cookie.min.js',
+        'https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router.min.js',
+        'https://unpkg.com/vuex@3.0.1/dist/vuex.min.js'
       ]
     }
   }
